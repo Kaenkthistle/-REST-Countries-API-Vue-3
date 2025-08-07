d23626d88ba3b34ab9e51ac3b181f1d942470831
kai_enko <kitteechftm@gmail.com>
2025-08-07 14:40:52 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/-REST-Countries-API-Vue-3/',
   plugins: [
     vue(),
     vueDevTools(),
